fbdb012d538ea971fa50ce70c8cb1117f632fa8f
Duca <vanderson.sampaio@yahoo.com.br>
2018-06-12 22:38:03 -0300

    Add Remove Files in util documents.

---
+++ src/util/document/RemoveFiles.java
@@ -0,0 +1,51 @@
+package util.document;
+
+import java.io.File;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class RemoveFiles {
+	
+	private static final String FILENAME = "C:\\Users\\Vanderson\\Dropbox\\Mestrado\\Dissertação\\Dados\\Sentimentos.txt";
+	private static final String PATH = "C:\\Users\\Vanderson\\Dropbox\\Mestrado\\Dissertação\\Dados\\Sentimentos\\DW_Alemao_Trans\\";
+	private static final String EXT = ".sents";
+
+	public static void main(String[] args) {
+		BufferedReader br = null;
+		FileReader fr = null;
+
+		try {
+			fr = new FileReader(FILENAME);
+			br = new BufferedReader(fr);
+
+			String lineFile;
+
+			while ((lineFile = br.readLine()) != null) {
+				System.out.println(lineFile);
+				
+				File file = new File(PATH + lineFile.trim() + EXT);
+				
+				if(file.isFile()){
+					file.delete();
+					System.out.println(file.getName() + " is deleted!");
+				} else {
+					System.out.println("Delete operation is failed.");
+				}
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (br != null)
+					br.close();
+
+				if (fr != null)
+					fr.close();
+
+			} catch (IOException ex) {
+				ex.printStackTrace();
+			}
+		}
+	}
+}
